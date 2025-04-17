660048e09955233b5c8833c39884160b7a9e38bc
Kusum462 <kusumakumarproject@.com>
2025-04-17 11:05:40 +0530

    Key

--- project1/settings.py
+++ project1/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django-insecure-%*g0q6v9b*(@$#btg=gd&oj5)wd(7n(6)cp%-5ooopa!%g%6k!
+
+
+SECRET_KEY = 'django-insecure-%*g0q6v9b*(@$#btg=gd&oj5)wd(7n(6)cp%-5ooopa!%g%6k!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
